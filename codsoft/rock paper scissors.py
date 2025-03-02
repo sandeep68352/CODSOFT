0c7937e93e2bfb04883f9a3a2bb214e581842a0b
Sandeep <123184104+sandeep68352@users.noreply.github.com>
2025-03-02 21:20:04 +0530

    Update rock paper scissors.py

--- codsoft/rock paper scissors.py
+++ codsoft/rock paper scissors.py
@@ -1,51 +1,31 @@
-
 import random
-print("""
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)
-""")
+game_over = False
+while not game_over :
 
-# Paper
-print("""
-     _______
----'    ____)____
-           ______)
-          _______)
-         _______)
----.__________)
-""")
-
-# Scissors
-print("""
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)
-""")
-game_image = ["rock,paper,scissors"]
-
-
-user_choice = int(input("what did you choose : 0 for rock ,1 for paper, 2 for sissors :"))
-print(f"user_choice{user_choice}:")
-print(game_image[user_choice])
-computer_choice = random.randint(0,2)
-print(f"computer_choice {computer_choice}")
-print(game_image[computer_choice])
-if user_choice == 0 and computer_choice == 2:
-    print("you win ")
-elif user_choice > computer_choice:
-    print("you win")
-elif computer_choice == 0 and user_choice == 2:
-    print("you loose")   
-elif computer_choice > user_choice :
-    print ("you loose")
-elif computer_choice == user_choice:
-    print("it's draw ")  
-else:
-    print(" you entered invaild input ")          
+    user_choice = input("what did you choose : 'rock' ,'paper', 'sissors' :").lower()
+    items = ('rock','paper','sissors')
+    print(f"user_choice : {user_choice}")
+    
+    computer_choice = random.choice(items)
+    print(f"computer_choice :{computer_choice}")
+    
+    if user_choice == "rock" and computer_choice == "sissors" :
+        print("you win ")
+    elif user_choice == "sissors" and computer_choice == "paper" : 
+        print(" you win ")  
+    elif user_choice == "paper" and computer_choice == "rock"  :
+        print("you win")      
+    elif computer_choice == "rock" and user_choice == "sissors" :
+        print("you loose")   
+    elif computer_choice == "sissors" and user_choice == "paper" :
+        print ("you loose ")  
+    elif computer_choice == "paper" and user_choice == "rock"  :
+        print("you loose")    
+    elif computer_choice == user_choice:
+        print("it's draw ")  
+    else:
+        print(" you entered invaild input ")   
+    stop_game = input("type 'y' for play again and 'n' for end the game :")
+    if stop_game == "n":
+        game_over = True
+        print("GAME END")
